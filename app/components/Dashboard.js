7bc583bf46ebf74eb67ddadafd2bdba3547e618a
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-05-09 01:45:29 -0400

    Added rating and example of tutors

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import StarRatingComponent from 'react-star-rating-component';
 
 import styles from '../styles/MainPage.css';
 
@@ -14,7 +15,79 @@ class MenuSelection extends Component {
 						<div className={styles.tabOption + " " + styles.tabOptionEnd}><i className="fas fa-graduation-cap"></i></div>
 					</div>
 					<div className={styles.dashboardList}>
-						list
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>
+									<StarRatingComponent
+					          name=""
+					          editing={false}
+					          starCount={5}
+					          value={3}
+					        />
+								</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+						<div className={styles.userListInnerSeparation}></div>
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>Ratings</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+						<div className={styles.userListInnerSeparation}></div>
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>Ratings</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+						<div className={styles.userListInnerSeparation}></div>
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>Ratings</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+						<div className={styles.userListInnerSeparation}></div>
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>Ratings</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+						<div className={styles.userListInnerSeparation}></div>
+						<div className={styles.tutorList}>
+							<div className={styles.userListFirstRow}>
+								<div className={styles.inlineUserStats}>Nom: Tiberiu Cristian Suciu</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>Ratings</div>
+							</div>
+							<div className={styles.userListInnerInfoSpacing}>
+								<div className={styles.inlineUserStats}>Niveau: Baccalauréat</div>
+								<div className={styles.inlineUserStats + " " + styles.userListRightAlign}>51 <i className="fas fa-user"></i></div>
+							</div>
+						</div>
+
 					</div>
 				</div>
 				<div className={styles.dashboardProfile}>
